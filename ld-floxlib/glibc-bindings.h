6c97f27a9d3c074efa0b4a3ef96a330e160c03cf
Michael Brantley <brantley@flox.dev>
2024-08-16 13:13:27 +0000

    chore: tweak glibc bindings

--- ld-floxlib/glibc-bindings.h
+++ ld-floxlib/glibc-bindings.h
@@ -1,10 +1,13 @@
+#ifndef GLIBC_BINDINGS_H
+#define GLIBC_BINDINGS_H
+
 // Declare version bindings to work with minimum supported GLIBC versions.
 //
 // This file needs to be updated whenever we start using a new GLIBC function.
 // The procedure to update the file is as follows:
 //
 //   make -C ld-floxlib libsandbox.so
-//   nm -D ld-floxlib/libsandbox.so | grep -v dlsym@ | \
+//   nm -D ld-floxlib/libsandbox.so | \
 //     sed 's/@GLIBC_.*/@GLIBC_MIN_VERSION/' | awk '/GLIBC/ {print $NF}' | \
 //     awk -F@ '{printf("__asm__( \".symver %s,%s@\" %s );\n",$1,$1,$2)}' >> ld-floxlib/glibc-bindings.h
 //   vi ld-floxlib/glibc-bindings.h
@@ -20,8 +23,8 @@
   #error "Unsupported architecture"
 #endif
 
-__asm__( ".symver dlsym,dlsym@" GLIBC_MIN_VERSION );
 __asm__( ".symver __cxa_finalize,__cxa_finalize@" GLIBC_MIN_VERSION );
+__asm__( ".symver dlsym,dlsym@" GLIBC_MIN_VERSION );
 __asm__( ".symver __errno_location,__errno_location@" GLIBC_MIN_VERSION );
 __asm__( ".symver fclose,fclose@" GLIBC_MIN_VERSION );
 __asm__( ".symver fgets,fgets@" GLIBC_MIN_VERSION );
@@ -42,3 +45,6 @@ __asm__( ".symver strcspn,strcspn@" GLIBC_MIN_VERSION );
 __asm__( ".symver strlen,strlen@" GLIBC_MIN_VERSION );
 __asm__( ".symver strncmp,strncmp@" GLIBC_MIN_VERSION );
 __asm__( ".symver strncpy,strncpy@" GLIBC_MIN_VERSION );
+__asm__( ".symver strtok_r,strtok_r@" GLIBC_MIN_VERSION );
+
+#endif // GLIBC_BINDINGS_H
